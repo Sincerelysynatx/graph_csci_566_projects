598164de8a28347124321a10e72f5a95ac6bcc5c
Sean <sean_pimentel@hotmail.com>
2017-09-07 14:54:29 -0700

    Finished commenting functions and tidying up code.
    Signed-off-by: Sean <sean_pimentel@hotmail.com>

--- a_1/a_1.js
+++ a_1/a_1.js
@@ -1,6 +1,15 @@
-// ColoredPoint.js (c) 2012 matsuda
-// Vertex shader program
+/*
+ * @author: Sean Pimentel
+ * @date: 08/7/2017
+ * @description: This program is supposed to simulate interpolation as well as
+ * demonstrate varying point sizes.
+ *
+ */
 
+/*
+ * Vertex Shader Code
+ * creating a position attribute and a pointer size attribute per vertex
+ */
 var VSHADER_SOURCE =
     'attribute vec4 a_Position;\n' +
     'attribute float psize;\n' +
@@ -9,7 +18,10 @@ var VSHADER_SOURCE =
     '  gl_PointSize = psize;\n' +
     '}\n';
 
-
+/*
+ * Fragment Shader Code
+ * Setting up a uniform fragment shader variable that does not change
+ */
 var FSHADER_SOURCE =
     'precision mediump float;\n' +
     'uniform vec4 u_FragColor;\n' +
@@ -17,6 +29,10 @@ var FSHADER_SOURCE =
     '  gl_FragColor = u_FragColor;\n' +
     '}\n';
 
+/*
+ * Main loop to get gl contex, attributes and uniforms from shaders, set mouse
+ * down and mouse up events, and clearing the canvas
+ */
 function main() {
     var canvas = document.getElementById('webgl');
 
@@ -53,7 +69,11 @@ function main() {
 
     canvas.onmousedown = function(ev){ click(ev, canvas, timer) };
 
-    canvas.onmouseup = function(){ clickUp(gl, a_Position, u_FragColor, psize, timer) };
+    canvas.onmouseup = function(){ clickUp(gl,
+                                            a_Position,
+                                            u_FragColor,
+                                            psize,
+                                            timer) };
 
     // Specify the color for clearing <canvas>
     gl.clearColor(0.0, 0.0, 0.0, 1.0);
@@ -64,9 +84,20 @@ function main() {
 
 var g_points = [];
 var g_colors = [];
+var g_psize = [];
+
+/*
+ * Function call for a mouse down event
+ *
+ * This function adds to the g_points and g_colors array for adding a point
+  * to be rendered. Determines the location of the point in our viewing area.
+ *
+ * @param ev for event from mouse click
+ * @param canvas element
+ * @param timer for starting the time on mouse down
+ */
 function click(ev, canvas, timer) {
     timer.time = Date.now();
-    console.log(timer.time);
 
     var x = ev.clientX;
     var y = ev.clientY;
@@ -77,7 +108,6 @@ function click(ev, canvas, timer) {
 
     // Store the coordinates to g_points array
     g_points.push([x, y]);
-    console.log(x, y);
 
     var r = ((-.5*x)+.5)/2 + ((.5*y)+.5)/2;
     var g = ((.5*x)+.5)/2 + ((.5*y)+.5)/2;
@@ -94,9 +124,29 @@ function click(ev, canvas, timer) {
     g_colors.push([r, g, b, a]);
 }
 
+
+/*
+ * Function call for mouse up event
+ *
+ * This function sets the ending time for the timer and uses a simple
+  * algorithm to determine the size of the point. Also this function is
+   * responsible for rendering out the points to the canvas through the gl
+    * context.
+ *
+ * @param gl context
+ * @param a_Position identifier for the space in hardware to set a vertex
+  * position
+ * @param u_FragColor identifier for the space in hardware to set a fragment
+  * color
+ * @param psize identifier for the space in hardware to set a pixel size
+ * @param timer used to finish a time stamp and determine how large the
+  * pixel should be
+ */
 function clickUp(gl, a_Position, u_FragColor, psize, timer) {
-    var duration = Date.now() - timer.time
+    var duration = Date.now() - timer.time;
     console.log(duration);
+
+    g_psize.push(duration/8);
     // Clear <canvas>
     gl.clear(gl.COLOR_BUFFER_BIT);
 
@@ -104,11 +154,12 @@ function clickUp(gl, a_Position, u_FragColor, psize, timer) {
     for(var i = 0; i < len; i++) {
         var xy = g_points[i];
         var rgba = g_colors[i];
+        var point_size = g_psize[i];
 
         // Pass the position of a point to a_Position variable
         gl.vertexAttrib3f(a_Position, xy[0], xy[1], 0.0);
-
-        gl.vertexAttrib1f(psize, duration/50.0);
+        // Pass the pixel size of a point to psize variable
+        gl.vertexAttrib1f(psize, point_size);
         // Pass the color of a point to u_FragColor variable
         gl.uniform4f(u_FragColor, rgba[0], rgba[1], rgba[2], rgba[3]);
         // Draw
