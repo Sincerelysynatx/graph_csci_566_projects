79285c39ab936298bc4c884ad48ddfe16a0308aa
Sean <sean_pimentel@hotmail.com>
2017-09-07 11:20:13 -0700

    Sped up the poing chaning size
    Signed-off-by: Sean <sean_pimentel@hotmail.com>

--- a_1/a_1.js
+++ a_1/a_1.js
@@ -108,7 +108,7 @@ function clickUp(gl, a_Position, u_FragColor, psize, timer) {
         // Pass the position of a point to a_Position variable
         gl.vertexAttrib3f(a_Position, xy[0], xy[1], 0.0);
 
-        gl.vertexAttrib1f(psize, duration/100.0);
+        gl.vertexAttrib1f(psize, duration/50.0);
         // Pass the color of a point to u_FragColor variable
         gl.uniform4f(u_FragColor, rgba[0], rgba[1], rgba[2], rgba[3]);
         // Draw
