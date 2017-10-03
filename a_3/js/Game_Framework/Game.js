523cad65849f8771433458f0eec0b0b78dff763f
Sean Pimentel <sean_pimentel@hotmail.com>
2017-10-03 10:23:40 -0700

    Started assignment 3
    Signed-off-by: Sean Pimentel <sean_pimentel@hotmail.com>

---
+++ a_3/js/Game_Framework/Game.js
@@ -0,0 +1,12 @@
+// var  = new World();
+
+function main(){
+    var canvas = document.getElementById('webgl');
+    var gl = getWebGLContext(canvas);
+    if (!gl) {
+        console.log('Failed to get the rendering context for WebGL');
+        return;
+    }
+
+    var world = new World();
+}
---
+++ a_3/js/Game_Framework/Player.js
@@ -0,0 +1,5 @@
+function Player(){
+    // this.model_verticies = new Float32Array([]);
+    this.x = 0.0;
+    this.y = 0.0;
+}
