9dcc9255e21f097e1e64ae415b1b89f13cb85310
Sean <sean_pimentel@hotmail.com>
2017-11-15 22:26:37 -0800

    About to split up mvp mat into proj mat and view mat
    Signed-off-by: Sean <sean_pimentel@hotmail.com>

--- a_5/a_5.js
+++ a_5/a_5.js
@@ -1,10 +1,12 @@
 // Vertex shader for solid drawing
 var SOLID_VSHADER_SOURCE =
     'attribute vec4 a_Position;\n' +
+    'attribute vec4 a_Color;\n' +
     'uniform mat4 u_MvpMatrix;\n' +
     'varying vec4 v_Color;\n' +
     'void main() {\n' +
     '   gl_Position = u_MvpMatrix * a_Position;\n' +
+    '   v_Color = a_Color;\n' +
     '}\n';
 
 // Fragment shader for solid drawing
@@ -40,6 +42,8 @@ var TEXTURE_FSHADER_SOURCE =
     '   gl_FragColor = vec4(color.rgba);\n' +
     '}\n';
 
+var camera;
+var viewProjMatrix;
 
 function main(){
     var canvas = document.getElementById('webgl');
@@ -60,6 +64,7 @@ function main(){
 
     // Get storage locations of attribute and uniform variables in program object for single color drawing
     solidProgram.a_Position = gl.getAttribLocation(solidProgram, 'a_Position');
+    solidProgram.a_Color = gl.getAttribLocation(solidProgram, 'a_Color');
     solidProgram.u_MvpMatrix = gl.getUniformLocation(solidProgram, 'u_MvpMatrix');
 
     // Get storage locations of attribute and uniform variables in program object for texture drawing
@@ -68,7 +73,7 @@ function main(){
     texProgram.u_MvpMatrix = gl.getUniformLocation(texProgram, 'u_MvpMatrix');
     texProgram.u_Sampler = gl.getUniformLocation(texProgram, 'u_Sampler');
 
-    if (solidProgram.a_Position < 0 || !solidProgram.u_MvpMatrix ||
+    if (solidProgram.a_Position < 0 || !solidProgram.u_MvpMatrix || !solidProgram.a_Color ||
         texProgram.a_Position < 0 || texProgram.a_TexCoord < 0 ||
         !texProgram.u_MvpMatrix || !texProgram.u_Sampler) {
         console.log('Failed to get the storage location of attribute or uniform variable');
@@ -76,6 +81,7 @@ function main(){
     }
 
     var ground_block = initVertexBuffers(gl, 100.0);
+    var block = initVertexBuffers(gl, 1.0);
 
     if (!ground_block){
         console.log('Failed to set the vertex information');
@@ -89,15 +95,38 @@ function main(){
     }
 
     gl.enable(gl.DEPTH_TEST);
-    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+    gl.clearColor(0.019, 0.603, 0.956, 1.0);
 
-    var viewProjMatrix = new Matrix4();
+    document.onkeydown = function(e){ moveCamera(e, camera, viewProjMatrix)};
+
+    camera = {
+        dir: {
+            x: 0.0
+            ,z: -1.0
+        }
+        ,pos: {
+            x: 0.0
+            ,z: 1.0
+        }
+        ,angle: 270
+    };
+
+    viewProjMatrix = new Matrix4();
     viewProjMatrix.setPerspective(60.0, canvas.width/canvas.height, 1.0, 100.0);
-    viewProjMatrix.lookAt(0.0, 0.0, 10.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
+    viewProjMatrix.lookAt(camera.pos.x, 0.0, camera.pos.z, camera.pos.x + camera.dir.x, 0.0, camera.pos.z + camera.dir.z, 0.0, 1.0, 0.0);
 
-    var cube1 = {
-         scale: new Vector3([2.0, 1.0, 1.0])
-        ,trans: new Vector3([-2.0, 0.0, 0.0])
+    var windmill = {
+        location: new Vector3([0.0, 0.0, 0.0])
+        ,base: {
+            scale: new Vector3([0.2, 2.0, 0.2])
+            ,trans: new Vector3([0.0, 0.0, -10.0])
+            ,color: new Vector3([1.0, 0.0, 0.0])
+        }
+        ,fan1: {
+            scale: new Vector3([1.5, 0.2, 0.1])
+            ,trans: new Vector3([0.0, 2.0, -10.0])
+            ,color: new Vector3([0.0, 1.0, 0.0])
+        }
     };
 
     var ground = {
@@ -116,10 +145,15 @@ function main(){
 
         gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
-        // console.log(cube1.trans);
+        // Draw ground
+        drawTexCube(gl, texProgram, ground_block, texture, ground.trans, ground.scale, 0, viewProjMatrix);
+
+        // Draw Windmill
+        drawSolidCube(gl, solidProgram , block, windmill.base.trans, windmill.base.scale, windmill.base.color, windmillRotation, viewProjMatrix);
+        drawSolidCube(gl, solidProgram , block, windmill.fan1.trans, windmill.fan1.scale, windmill.fan1.color, windmillRotation, viewProjMatrix);
 
         // drawSolidCube(gl, solidProgram, cube, cube1.trans, cube1.scale, windmillRotation, viewProjMatrix);
-        drawTexCube(gl, texProgram, ground_block, texture, ground.trans, ground.scale, 0, viewProjMatrix);
+        // console.log(cube1.trans);
         // drawSolidCube(gl, solidProgram, cube, -2.0, windmillRotation, viewProjMatrix);
         // drawTexCube(gl, texProgram, cube, texture, 2.0, windmillRotation, viewProjMatrix);
 
@@ -129,45 +163,41 @@ function main(){
     tick();
 }
 
+
+function moveCamera(e, camera, viewProjMatrix) {
+
+    e = e || window.event;
+
+    if (e.keyCode == '38') {
+        // up arrow
+    }
+    else if (e.keyCode == '40') {
+        // down arrow
+    }
+    else if (e.keyCode == '37') {
+        camera.angle -= 1;
+        camera.angle %= 360;
+        // left arrow
+    }
+    else if (e.keyCode == '39') {
+        camera.angle += 1;
+        camera.angle %= 360;
+        // right arrow
+    }
+
+    camera.dir.x = Math.cos(camera.angle * Math.PI / 180);
+    camera.dir.z = Math.sin(camera.angle * Math.PI / 180);
+
+    viewProjMatrix.lookAt(0.0, 0.0, 0.0,
+        camera.dir.x, 0.0, camera.dir.z,
+        0.0, 1.0, 0.0);
+
+    // gl.uniformMatrix4fv(solidProgram.u_MvpMatrix, false, viewProjMatrix.elements);
+    // gl.uniformMatrix4fv(texProgram.u_MvpMatrix, false, viewProjMatrix.elements);
+
+}
+
 function initVertexBuffers(gl, scale){
-    // var verticies = new Float32Array ([
-    //     -0.5, 0.5, 0.5,
-    //     -0.5, -0.5, 0.5,
-    //     0.5, -0.5, 0.5,
-    //     -0.5, 0.5, 0.5,
-    //     0.5, -0.5, 0.5,
-    //     0.5, 0.5, 0.5,
-    //     0.5, 0.5, 0.5,
-    //     0.5, -0.5, 0.5,
-    //     0.5, -0.5, -0.5,
-    //     0.5, 0.5, 0.5,
-    //     0.5, -0.5, -0.5,
-    //     0.5, 0.5, -0.5,
-    //     0.5, -0.5, 0.5,
-    //     -0.5, -0.5, 0.5,
-    //     -0.5, -0.5, -0.5,
-    //     0.5, -0.5, 0.5,
-    //     -0.5, -0.5, -0.5,
-    //     0.5, -0.5, -0.5,
-    //     0.5, 0.5, -0.5,
-    //     -0.5, 0.5, -0.5,
-    //     -0.5, 0.5, 0.5,
-    //     0.5, 0.5, -0.5,
-    //     -0.5, 0.5, 0.5,
-    //     0.5, 0.5, 0.5,
-    //     -0.5, -0.5, -0.5,
-    //     -0.5, 0.5, -0.5,
-    //     0.5, 0.5, -0.5,
-    //     -0.5, -0.5, -0.5,
-    //     0.5, 0.5, -0.5,
-    //     0.5, -0.5, -0.5,
-    //     -0.5, 0.5, -0.5,
-    //     -0.5, -0.5, -0.5,
-    //     -0.5, -0.5, 0.5,
-    //     -0.5, 0.5, -0.5,
-    //     -0.5, -0.5, 0.5,
-    //     -0.5, 0.5, 0.5
-    // ]);
 
     var verticies = new Float32Array([   // Vertex coordinates
         1.0, 1.0, 1.0,  -1.0, 1.0, 1.0,  -1.0,-1.0, 1.0,   1.0,-1.0, 1.0,    // v0-v1-v2-v3 front
@@ -187,6 +217,15 @@ function initVertexBuffers(gl, scale){
         0.0, 0.0,   scale, 0.0,   scale, scale,   0.0, scale     // v4-v7-v6-v5 back
     ]);
 
+    var colors = new Float32Array([     // Colors
+        0.4, 0.4, 1.0,  0.4, 0.4, 1.0,  0.4, 0.4, 1.0,  0.4, 0.4, 1.0,  // v0-v1-v2-v3 front(blue)
+        0.4, 1.0, 0.4,  0.4, 1.0, 0.4,  0.4, 1.0, 0.4,  0.4, 1.0, 0.4,  // v0-v3-v4-v5 right(green)
+        1.0, 0.4, 0.4,  1.0, 0.4, 0.4,  1.0, 0.4, 0.4,  1.0, 0.4, 0.4,  // v0-v5-v6-v1 up(red)
+        1.0, 1.0, 0.4,  1.0, 1.0, 0.4,  1.0, 1.0, 0.4,  1.0, 1.0, 0.4,  // v1-v6-v7-v2 left
+        1.0, 1.0, 1.0,  1.0, 1.0, 1.0,  1.0, 1.0, 1.0,  1.0, 1.0, 1.0,  // v7-v4-v3-v2 down
+        0.4, 1.0, 1.0,  0.4, 1.0, 1.0,  0.4, 1.0, 1.0,  0.4, 1.0, 1.0   // v4-v7-v6-v5 back
+    ]);
+
     var indices = new Uint8Array([        // Indices of the vertices
         0, 1, 2,   0, 2, 3,    // front
         4, 5, 6,   4, 6, 7,    // right
@@ -200,8 +239,9 @@ function initVertexBuffers(gl, scale){
     var o = new Object();
     o.vertexBuffer = initArrayBufferForLaterUse(gl, verticies, 3, gl.FLOAT);
     o.texCoordBuffer = initArrayBufferForLaterUse(gl, texCoords, 2, gl.FLOAT);
+    o.colorBuffer = initArrayBufferForLaterUse(gl, colors, 3, gl.FLOAT);
     o.indexBuffer = initElementArrayBufferForLaterUse(gl, indices, gl.UNSIGNED_BYTE);
-    if (!o.vertexBuffer || !o.texCoordBuffer || !o.indexBuffer)
+    if (!o.vertexBuffer || !o.texCoordBuffer || !o.indexBuffer || !o.colorBuffer)
         return null;
 
     o.numIndices = indices.length;
@@ -248,12 +288,16 @@ function initTextures(gl, program){
     return texture;
 }
 
-function drawSolidCube(gl, program, o, trans, scale, angle, viewProjMatrix){
+function drawSolidCube(gl, program, o, trans, scale, color, angle, viewProjMatrix){
     gl.useProgram(program);   // Tell that this program object is used
 
     // Assign the buffer objects and enable the assignment
     initAttributeVariable(gl, program.a_Position, o.vertexBuffer); // Vertex coordinates
+    initAttributeVariable(gl, program.a_Color, o.colorBuffer);
+    gl.uniformMatrix4fv(program.u_MvpMatrix, false, viewProjMatrix.elements);
     gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, o.indexBuffer);  // Bind indices
+
+    // gl.uniform4f(program.u_Color, color.elements[0], color.elements[1], color.elements[2], 1);
 
     drawCube(gl, program, o, trans, scale, angle, viewProjMatrix);   // Draw
 }
@@ -276,20 +320,12 @@ function drawTexCube(gl, program, o, texture, trans, scale, angle, viewProjMatri
 // Coordinate transformation matrix
 var g_modelMatrix = new Matrix4();
 var g_mvpMatrix = new Matrix4();
-var g_normalMatrix = new Matrix4();
 
 function drawCube(gl, program, o, trans, scale, angle, viewProjMatrix) {
     // Calculate a model matrix
     g_modelMatrix.setTranslate(trans.elements[0], trans.elements[1], trans.elements[2]);
-    // g_modelMatrix.setTranslate(x, 0.0, 0.0);
-    // g_modelMatrix.rotate(20.0, 1.0, 0.0, 0.0);
     g_modelMatrix.rotate(angle, 0.0, 1.0, 0.0);
     g_modelMatrix.scale(scale.elements[0], scale.elements[1], scale.elements[2]);
-
-    // Calculate transformation matrix for normals and pass it to u_NormalMatrix
-    g_normalMatrix.setInverseOf(g_modelMatrix);
-    g_normalMatrix.transpose();
-    gl.uniformMatrix4fv(program.u_NormalMatrix, false, g_normalMatrix.elements);
 
     // Calculate model view projection matrix and pass it to u_MvpMatrix
     g_mvpMatrix.set(viewProjMatrix);
